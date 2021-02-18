60cd28d9b68d1b7d9bb2cb2c5d4864baa8f6891b
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2021-02-18 22:01:38 +0100

    Remove default initializer for serial port

--- lib/p1_meter_reader/data_parsing/stream_splitter.rb
+++ lib/p1_meter_reader/data_parsing/stream_splitter.rb
@@ -1,10 +1,9 @@
-require 'serialport'
 require 'io/wait'
 
 module P1MeterReader
   module DataParsing
     class StreamSplitter
-      def initialize(message_start, input: serial_port)
+      def initialize(message_start, input:)
         @message_start = message_start
         @stream = input
       end
@@ -25,16 +24,6 @@ module P1MeterReader
 
       def ready?
         @stream.ready?
-      end
-
-      private
-      def serial_port
-        serial_port = SerialPort.new("/dev/ttyUSB0", 9600)
-        serial_port.data_bits = 7
-        serial_port.stop_bits = 1
-        serial_port.parity = SerialPort::EVEN
-
-        serial_port
       end
     end
   end
